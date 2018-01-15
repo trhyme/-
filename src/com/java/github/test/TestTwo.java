c1891154b8ad9c1c6338cd6f0abf86102ad1f7cd
trhyme <2419891580@qq.com>
2018-01-15 15:25:48 +0800

    新增测试Class

---
+++ src/com/java/github/test/TestTwo.java
@@ -0,0 +1,13 @@
+package com.java.github.test;
+
+/***
+ * 
+ * @author Trhyme
+ * GitHub测试
+ */
+public class TestTwo {
+	public static void main(String[] args) {
+		int a=1,b=0;
+		System.out.println("Add:"+a+b);
+	}
+}
